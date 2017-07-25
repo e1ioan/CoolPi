96a4ce02cdd03897420e08c7c93fef1734fa9d52
Ioan Ghip <e1ioan@users.noreply.github.com>
2017-07-25 09:46:18 -0700

    Add files via upload

---
+++ coolPi.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Get a Pi cooling fan, 5V - 200mA and follow the schematic from here:
+# https://github.com/e1ioan/CoolPi
+
+
+#https://www.quora.com/How-can-we-control-the-speed-of-the-fan-using-Raspberry-pi-2
+#To generate a hardware pwm
+
+#pwmFrequency in Hz = 19.2e6 Hz / pwmClock / pwmRange
+
+#To generate a 50 hz signal
+#50 Hz = 19.2e6 Hz / 1920 / 200
+
+GPIO_PWM=26
+
+#set gpio 26 as pwm
+gpio mode $GPIO_PWM pwm  
+gpio pwm-ms
+#pwm clock
+gpio pwmc 1920
+#pwm range
+gpio pwmr 200
+# generate pwm on gpio 26 of 1/4 th duty cycle cause 50/200=1/4
+# gpio pwm 26 50
+
+while true
+do
+	temp=$(cat /sys/class/thermal/thermal_zone0/temp)
+	cpuTemp=$(($temp/1000))
+	if [[ $cpuTemp -lt 80 ]]; then
+		# stop fan
+		gpio pwm $GPIO_PWM 0
+	elif [ $cpuTemp -ge 80 ] && [ $cpuTemp -lt 100 ]; then
+		# fan at 50% speed
+		gpio pwm $GPIO_PWM 100
+	elif [[ $cpuTemp -ge 100 ]]; then
+        gpio pwm $GPIO_PWM 200
+	fi
+	# wait 10 seconds
+	sleep 10
+done
